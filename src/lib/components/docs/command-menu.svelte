fc3fcc3ab085ae41ff2502bdd3c23713b2e4f81e
minimo-io <nicolas@minimo.io>
2024-07-02 09:16:57 -0300

    Corrections!

--- src/lib/components/docs/command-menu.svelte
+++ src/lib/components/docs/command-menu.svelte
@@ -44,7 +44,7 @@
 	{...$$restProps}
 >
 	<span class="hidden lg:inline-flex"> Procurar sobre IA... </span>
-	<span class="inline-flex lg:hidden">Search...</span>
+	<span class="inline-flex lg:hidden">Procurar...</span>
 	<kbd
 		class="pointer-events-none absolute right-1.5 top-1.5 hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex"
 	>
--- src/lib/components/docs/metadata.svelte
+++ src/lib/components/docs/metadata.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import { dev } from "$app/environment";
-	import { page } from "$app/stores";
-	import { siteConfig } from "$lib/config/site.js";
+	import { dev } from '$app/environment';
+	import { page } from '$app/stores';
+	import { siteConfig } from '$lib/config/site.js';
 
 	export let title: string = siteConfig.name;
 
@@ -17,26 +17,19 @@
 	<meta name="twitter:site" content={siteConfig.url} />
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={siteConfig.description} />
-	<meta name="twitter:image" content="https://shadcn-svelte.com/og.png" />
+	<meta name="twitter:image" content="/og.png" />
 	<meta name="twitter:image:alt" content={siteConfig.name} />
 	<meta name="twitter:creator" content="huntabyte" />
 	<meta property="og:title" content={title} />
 	<meta property="og:type" content="article" />
 	<meta property="og:url" content={siteConfig.url + $page.url.pathname} />
-	<meta property="og:image" content="https://shadcn-svelte.com/og.png" />
+	<meta property="og:image" content="/og.png" />
 	<meta property="og:image:alt" content={siteConfig.name} />
 	<meta property="og:image:width" content="1200" />
 	<meta property="og:image:height" content="630" />
 	<meta property="og:description" content={siteConfig.description} />
 	<meta property="og:site_name" content={siteConfig.name} />
-	<meta property="og:locale" content="EN_US" />
+	<meta property="og:locale" content="pt_BR" />
 	<link rel="shortcut icon" href="/favicon-16x16.png" />
 	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-	{#if !dev}
-		<script
-			defer
-			data-domain="shadcn-svelte.com"
-			src="https://server.hj.run/js/script.js"
-		></script>
-	{/if}
 </svelte:head>
