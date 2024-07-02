cf91e8e4ff413e11a6d9aa6947006016713ad5dd
minimo-io <nicolas@minimo.io>
2024-07-02 19:56:30 -0300

    Supabase added to the project!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,15 @@
 	import * as PageHeader from '$lib/components/docs/page-header/index.js';
 	import { Icons } from '$lib/components/docs/icons/index.js';
 	import { buttonVariants } from '$lib/registry/new-york/ui/button/index.js';
-	import { siteConfig } from '$lib/config/site.js';
 	import { cn } from '$lib/utils.js';
+
+	export let data;
+	$: ({ countries } = data);
 </script>
 
-<div class="container relative z-10">
+<div class="container relative">
 	<PageHeader.Root class="pb-8">
-		<Announcement />
+		<Announcement class="z-10" />
 		<PageHeader.Heading>
 			<!-- Build your component library -->
 			<!-- IA para alavancar a sua empresa -->
@@ -27,7 +29,7 @@
 			<!-- Vamos além do medo? -->
 		</PageHeader.Description>
 
-		<div class="flex w-full items-center justify-center space-x-4 py-4 md:pb-10">
+		<div class="z-10 flex w-full items-center justify-center space-x-4 py-4 md:pb-10">
 			<iframe
 				title="Suscribase"
 				src="https://embeds.beehiiv.com/a4e57abb-ef8e-4d65-b939-ca8d2ca7d243?slim=true"
@@ -37,7 +39,11 @@
 				scrolling="no"
 				style="margin: 0; border-radius: 6px !important; background-color: transparent;width:90%;"
 			></iframe>
-
+			<ul class="hidden">
+				{#each countries as country}
+					<li>{country.name}</li>
+				{/each}
+			</ul>
 			<!-- <a href="/assine" class={cn(buttonVariants())}> Começar agora </a>
 			<a
 				target="_blank"
